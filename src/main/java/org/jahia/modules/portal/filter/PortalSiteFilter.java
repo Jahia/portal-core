27d4042b6eb8e7e303b4a8690be74d8469936e62
jkevan <kevan@jahia.com>
2016-07-05 18:13:30 +0200

    QA-8633: fix PortalSiteFilter to avoid reading JCR node before the cache, use main resource for condition.

--- src/main/java/org/jahia/modules/portal/filter/PortalSiteFilter.java
+++ src/main/java/org/jahia/modules/portal/filter/PortalSiteFilter.java
@@ -46,7 +46,6 @@ package org.jahia.modules.portal.filter;
 import org.jahia.modules.portal.PortalConstants;
 import org.jahia.modules.portal.service.PortalService;
 import org.jahia.services.content.JCRContentUtils;
-import org.jahia.services.content.JCRNodeWrapper;
 import org.jahia.services.render.RenderContext;
 import org.jahia.services.render.Resource;
 import org.jahia.services.render.filter.AbstractFilter;
@@ -64,11 +63,10 @@ public class PortalSiteFilter extends AbstractFilter{
 
     @Override
     public String prepare(RenderContext renderContext, Resource resource, RenderChain chain) throws Exception {
-        JCRNodeWrapper portalTabNode = renderContext.getMainResource().getNode().isNodeType(PortalConstants.JNT_PORTAL_TAB)
-                ? renderContext.getMainResource().getNode() : JCRContentUtils.getParentOfType(renderContext.getMainResource().getNode(), PortalConstants.JNT_PORTAL_TAB);
-
-        portalService.fixPortalSiteInContext(renderContext, portalTabNode.getParent());
-        
+        if(renderContext.getMainResource().getNode().isNodeType("jmix:portalContextWatcher")) {
+            portalService.fixPortalSiteInContext(renderContext,
+                    JCRContentUtils.getParentOfType(renderContext.getMainResource().getNode(), PortalConstants.JMIX_PORTAL));
+        }
         return null;
     }
 }
